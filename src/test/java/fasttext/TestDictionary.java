4b3e9d994c478e8b0f9ddb8fd1ee8f6dce7c612a
ivanhk <ivanhk@gmail.com>
2017-01-19 12:38:37 +0800

    TestDictionary add test case

--- src/test/java/fasttext/TestDictionary.java
+++ src/test/java/fasttext/TestDictionary.java
@@ -2,12 +2,14 @@ package fasttext;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
 import org.junit.Test;
 
 public class TestDictionary {
 
 	Dictionary dictionary = new Dictionary(new Args());
-	
+
 	@Test
 	public void testHash() {
 		assertEquals(dictionary.hash(","), 688690635l);
@@ -20,5 +22,20 @@ public class TestDictionary {
 		assertEquals(dictionary.find(","), 28690635l);
 		assertEquals(dictionary.find("is"), 22329493l);
 		assertEquals(dictionary.find("</s>"), 17362777l);
+	}
+
+	@Test
+	public void testAdd() {
+		dictionary.add(",");
+		dictionary.add("is");
+		dictionary.add("is");
+		String w = "";
+		dictionary.add(w);
+		dictionary.add(w);
+		dictionary.add(w);
+		Map<Long, Integer> word2int = dictionary.getWord2int();
+		assertEquals(3, dictionary.getWords().get(word2int.get(dictionary.find(w))).count);
+		assertEquals(2, dictionary.getWords().get(word2int.get(dictionary.find("is"))).count);
+		assertEquals(1, dictionary.getWords().get(word2int.get(dictionary.find(","))).count);
 	}
 }
